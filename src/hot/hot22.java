e9e87a053e2abe687c7af8b2a0d5e33ed69d56fc
wang <1227883481@qq.com>
2021-06-16 21:32:20 +0800

    力扣hot-扎气球（贪心算法）

---
+++ src/hot/hot22.java
@@ -0,0 +1,15 @@
+package hot;
+
+/**
+ * @author wgl
+ * @create 2021-06-13 12:49
+ */
+public class hot22 {
+    public static void main(String[] args) {
+        StringBuilder stringBuilder = new StringBuilder();
+        System.out.println(stringBuilder.length());
+//        stringBuilder.insert(3, "()");
+        System.out.println(stringBuilder.toString());
+
+    }
+}
