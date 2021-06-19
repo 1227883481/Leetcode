6788c9f346b161b3e55c28c40fe73f276fa15a48
wang <1227883481@qq.com>
2021-06-19 15:32:07 +0800

    力扣hot-合并三个有序链表 归并排序的思路

---
+++ src/hot/hot38.java
@@ -0,0 +1,54 @@
+package hot;
+
+/**
+ * @author wgl
+ * @create 2021-06-19 15:30
+ */
+public class hot38 {
+    public static void main(String[] args) {
+
+    }
+    public ListNode mergeKLists(ListNode[] lists) {
+        if (lists == null || lists.length == 0) {
+            return null;
+        }
+        int n = lists.length;
+        for (int i = 1; i < n; i = i * 2) {
+            for (int start = 0; start < n; start = start + 2 * i) {
+                if (start + i >= n) {
+                    break;
+                }
+                ListNode head1 = lists[start];
+                ListNode head2 = lists[start + i];
+                ListNode head = new ListNode(0);
+                ListNode cur1 = head1;
+                ListNode cur2 = head2;
+                ListNode cur = head;
+                while (cur1 != null && cur2 != null) {
+                    if (cur1.val < cur2.val) {
+                        cur.next = cur1;
+                        cur1 = cur1.next;
+                    } else {
+                        cur.next = cur2;
+                        cur2 = cur2.next;
+                    }
+                    cur = cur.next;
+                }
+                if (cur1 == null) {
+                    cur.next = cur2;
+                } else {
+                    cur.next = cur1;
+                }
+                lists[start] = head.next;
+            }
+        }
+        return lists[0];
+    }
+}
+class ListNode {
+      int val;
+      ListNode next;
+      ListNode() {}
+      ListNode(int val) { this.val = val; }
+      ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+  }
