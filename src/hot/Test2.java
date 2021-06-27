f9b3831a485b003553237e9f6bb7c8a5e457422e
wang <1227883481@qq.com>
2021-06-27 23:18:02 +0800

    力扣hot-数组与list方法测试

---
+++ src/hot/Test2.java
@@ -0,0 +1,17 @@
+package hot;
+
+/**
+ * @author wgl
+ * @create 2021-06-22 16:02
+ */
+public class Test2 {
+    TreeNode node1;
+    TreeNode node2;
+    public static void main(String[] args) {
+        TreeNode node1 = null;
+        TreeNode node2 = null;
+        if (node1 == node2) {
+            System.out.println(node1);
+        }
+    }
+}
