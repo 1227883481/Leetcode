967cc25831d1816aab0e98fab3c736994e3ec78f
wang <1227883481@qq.com>
2021-06-30 21:59:48 +0800

    力扣hot-课程表-有向图做法

---
+++ src/hot/hot207.java
@@ -0,0 +1,39 @@
+package hot;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Queue;
+
+/**
+ * @author wgl
+ * @create 2021-06-30 21:58
+ */
+public class hot207 {
+    public static void main(String[] args) {
+
+    }
+    public boolean canFinish(int numCourses, int[][] prerequisites) {
+        int[] indegrees = new int[numCourses];
+        List<List<Integer>> adjacency = new ArrayList<>();
+        Queue<Integer> queue = new LinkedList<>();
+        for(int i = 0; i < numCourses; i++)
+            adjacency.add(new ArrayList<>());
+        // Get the indegree and adjacency of every course.
+        for(int[] cp : prerequisites) {
+            indegrees[cp[0]]++;
+            adjacency.get(cp[1]).add(cp[0]);
+        }
+        // Get all the courses with the indegree of 0.
+        for(int i = 0; i < numCourses; i++)
+            if(indegrees[i] == 0) queue.add(i);
+        // BFS TopSort.
+        while(!queue.isEmpty()) {
+            int pre = queue.poll();
+            numCourses--;
+            for(int cur : adjacency.get(pre))
+                if(--indegrees[cur] == 0) queue.add(cur);
+        }
+        return numCourses == 0;
+    }
+}
