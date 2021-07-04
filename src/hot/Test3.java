c30ab9bd23108ee344ee1e47e278cc455c84ff2a
wang <1227883481@qq.com>
2021-07-05 00:32:04 +0800

    力扣hot-小实验

---
+++ src/hot/Test3.java
@@ -0,0 +1,15 @@
+package hot;
+
+import java.util.Collection;
+import java.util.HashSet;
+
+/**
+ * @author wgl
+ * @create 2021-07-03 15:19
+ */
+public class Test3 {
+    public static void main(String[] args) {
+        Collection<String> coll = new HashSet<>();
+        coll.add("a");
+    }
+}
