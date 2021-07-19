907cfc22da6f9330cd97329a2746bba116533596
wang <1227883481@qq.com>
2021-07-19 19:09:24 +0800

    力扣-test

---
+++ src/labuladong/Test1.java
@@ -0,0 +1,19 @@
+package labuladong;
+
+/**
+ * @author wgl
+ * @create 2021-07-16 15:14
+ */
+public class Test1 {
+    public static void main(String[] args) {
+        Integer int1 = 25700;
+        Integer int2 = 25700;
+        int a = 25700;
+        if (a == int2) {
+            System.out.println("相等");
+        }
+        if (int1.equals(int2)) {
+            System.out.println("equals");
+        }
+    }
+}
