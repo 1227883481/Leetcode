9a923b3f56c53759965f2d0cad1f301860be45c3
wang <1227883481@qq.com>
2021-07-21 21:47:24 +0800

    力扣-除自身以外的乘积

--- src/labuladong/lc567.java
+++ src/labuladong/lc567.java
@@ -1,11 +1,28 @@
 package labuladong;
 
+import java.util.ArrayDeque;
+import java.util.Deque;
+import java.util.Queue;
+import java.util.Stack;
+
 /**
  * @author wgl
  * @create 2021-07-16 16:34
  */
 public class lc567 {
+    public static void main(String[] args) {
+        Stack<Integer> stack = new Stack<>();
+        stack.push(null);
+        if (stack.isEmpty()) {
+            System.out.println("空");
+        } else {
+            System.out.println(stack.pop());
+        }
+        Deque deque = new ArrayDeque();
+    }
     public int[] rob() {
+
         return new int[2];
+
     }
 }
