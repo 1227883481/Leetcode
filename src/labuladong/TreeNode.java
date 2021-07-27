e869f7aad4970e85e858edf8918393d631fd1f5f
wang <1227883481@qq.com>
2021-07-27 23:33:36 +0800

    力扣-树 节点

---
+++ src/labuladong/TreeNode.java
@@ -0,0 +1,18 @@
+package labuladong;
+
+/**
+ * @author wgl
+ * @create 2021-07-25 22:29
+ */
+public class TreeNode {
+    int val;
+      TreeNode left;
+      TreeNode right;
+      TreeNode() {}
+      TreeNode(int val) { this.val = val; }
+      TreeNode(int val, TreeNode left, TreeNode right) {
+          this.val = val;
+          this.left = left;
+          this.right = right;
+      }
+}
