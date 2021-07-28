3362adbd80cdbdffae7ec7a856534a98e374db5b
wang <1227883481@qq.com>
2021-07-28 23:47:20 +0800

    力扣-list

---
+++ src/labuladong/ListNode.java
@@ -0,0 +1,13 @@
+package labuladong;
+
+/**
+ * @author wgl
+ * @create 2021-07-24 23:10
+ */
+public class ListNode {
+    int val;
+    ListNode next;
+    ListNode() {}
+    ListNode(int val) { this.val = val; }
+    ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+}
