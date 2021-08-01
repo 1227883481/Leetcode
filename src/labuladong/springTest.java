140a4b836b1f60216d9be88b030768dac7bb890d
wang <1227883481@qq.com>
2021-08-01 17:19:39 +0800

    力扣-springtest

---
+++ src/labuladong/springTest.java
@@ -0,0 +1,13 @@
+package labuladong;
+import proxy.UserService;
+import proxy.UserServiceImpl;
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Proxy;
+
+/**
+ * @author wgl
+ * @create 2021-07-27 15:21
+ */
+public class springTest {
+    UserService proxy = (UserService) Proxy.newProxyInstance(classLoader, interfaces, logHandler);
+}
