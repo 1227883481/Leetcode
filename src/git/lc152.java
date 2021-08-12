26230351a9211ab59eeadc98296d4dd4b7519eec
wang <1227883481@qq.com>
2021-08-12 09:13:16 +0800

    力扣-乘积最大的子数组

---
+++ src/git/lc152.java
@@ -0,0 +1,41 @@
+package git;
+
+/**
+ * @author wgl
+ * @create 2021-08-12 9:12
+ */
+public class lc152 {
+    public int maxProduct(int[] nums) {
+        if (nums.length == 1) {
+            return nums[0];
+        }
+        int res = 0;
+        int pos = 0;
+        int nag = 0;
+        int cur = 0;
+        int posCur = 0;
+        int nagCur = 0;
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] > 0) {
+                cur = Math.max(pos * nums[i], nums[i]);
+                pos = cur;
+                nag = nag * nums[i];
+            }
+            if (nums[i] < 0) {
+                posCur = nag * nums[i];
+                nagCur = Math.min(pos * nums[i], nums[i]);
+                cur = posCur;
+                pos = posCur;
+                nag = nagCur;
+            }
+            if (nums[i] == 0) {
+                nag = 0;
+                pos = 0;
+            }
+            if (cur > res) {
+                res = cur;
+            }
+        }
+        return res;
+    }
+}
