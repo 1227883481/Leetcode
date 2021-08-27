9d7e9f4f2e795426cc522a5bc71484377d43ae1e
wang <1227883481@qq.com>
2021-08-27 16:36:07 +0800

    力扣-测试null

---
+++ src/experience/nullTest.java
@@ -0,0 +1,17 @@
+package experience;
+
+import labuladong.TreeNode;
+
+/**
+ * @author wgl
+ * @create 2021-08-27 16:34
+ */
+public class nullTest {
+    public static void main(String[] args) {
+        TreeNode A = null;
+        TreeNode B = null;
+        if (A == null) {
+            System.out.println(A);
+        }
+    }
+}
