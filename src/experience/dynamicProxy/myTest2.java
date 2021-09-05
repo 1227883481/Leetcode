b7216f40a14d6b0ecbc6c60c177226b2469a32c8
wang <1227883481@qq.com>
2021-09-05 14:37:00 +0800

    快排思路测试

---
+++ src/experience/dynamicProxy/myTest2.java
@@ -0,0 +1,40 @@
+package experience.dynamicProxy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author wgl
+ * @create 2021-09-04 10:47
+ */
+public class myTest2 {
+    public static void main(String[] args) {
+        myTest2 myTest2 = new myTest2();
+        System.out.println(myTest2.lengthOfLongestSubstring("abcababb"));
+    }
+    public int lengthOfLongestSubstring(String s) {
+        int res = 1;
+        Map<Character,Integer> map = new HashMap<>();
+        int left = 0;
+        int right = 0;
+        while (right < s.length()) {
+
+            while(left < right && map.getOrDefault(s.charAt(right), 0) > 0) {
+//                int countA = map.get(s.charAt(left)) - 1;
+                map.put(s.charAt(left), map.get(s.charAt(left)) - 1);
+                left++;
+                char c = s.charAt(right);
+//                System.out.println(s.charAt(right));
+                int temp = map.getOrDefault(c, 0);
+                System.out.println(temp);
+            }
+            map.put(s.charAt(right), 1);
+            if (map.size() > res) {
+                res = map.size();
+            }
+            right++;
+        }
+        return res;
+
+    }
+}
